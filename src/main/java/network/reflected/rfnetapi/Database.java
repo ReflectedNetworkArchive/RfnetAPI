0fda3d9f29e3d632bb7d67456166c6f61b0ad994
Sam Knight <robogamingbysam@icloud.com>
2021-04-29 20:30:15 -0500

    Finished configuration, untested

--- src/main/java/network/reflected/rfnetapi/Database.java
+++ src/main/java/network/reflected/rfnetapi/Database.java
@@ -6,9 +6,16 @@ import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
 import com.mongodb.client.MongoDatabase;
 import io.lettuce.core.RedisClient;
+import io.lettuce.core.RedisURI;
 import io.lettuce.core.api.StatefulRedisConnection;
 import org.bukkit.Bukkit;
 
+import java.time.Duration;
+
+// Abstracts away database so I can swap it out easier, since I
+// would only have to change code in here. Also takes in a config
+// to save the hassle of passing in a bunch of values that are
+// just straight from the config anyways.
 public class Database {
     private RedisClient redisClient;
     private StatefulRedisConnection<String, String> redisConnection;
@@ -20,7 +27,9 @@ public class Database {
     public Database(ServerConfig serverConfig) {
         this.serverConfig = serverConfig;
 
+        // In a try-catch because sometimes it fails. In the case that it does, connected will not be set. Yay!
         try {
+            // Mongo initialization
             ConnectionString connString = new ConnectionString(
                     serverConfig.getMongoURI()
             );
@@ -30,14 +39,25 @@ public class Database {
                     .build();
             mongoClient = MongoClients.create(settings);
             database = mongoClient.getDatabase("purchasedata");
+
+            // Lettuce initialization
             redisClient = RedisClient.create("");
-            redisConnection = redisClient.connect(serverConfig.getRedisURI());
+            redisConnection = redisClient.connect(
+                    new RedisURI(
+                            serverConfig.getRedisURI(),
+                            serverConfig.getRedisPort(),
+                            Duration.ofSeconds(5)
+                    )
+            );
+
+            // If the above has finished without error, we're connected!
             connected = false;
         } catch (Exception e) {
-            e.printStackTrace();
+            e.printStackTrace(); // Don't just swallow the error that would be bad ;)
         }
     }
 
+    // Getter.
     public boolean isConnected() {
         return connected;
     }
@@ -47,6 +67,21 @@ public class Database {
         if (available) {
             // Register this server by adding it to a set of server ids.
             redisConnection.sync().sadd("servers", serverConfig.getId());
+
+            // Register the settings for this type of server, and this server as a valid archetype
+            redisConnection.sync().sadd("archetypes", serverConfig.getArchetype());
+
+            redisConnection.sync().hset(
+                    "archetype:" + serverConfig.getArchetype(),
+                    "strategy",
+                    serverConfig.getConnectionStrategy()
+            );
+
+            redisConnection.sync().hset(
+                    "archetype:" + serverConfig.getArchetype(),
+                    "strategy",
+                    String.valueOf(serverConfig.isGlobalChatEnabled())
+            );
 
             // Creates a hash map with a name of server:<id> and fills it with
             // information about this instance.
@@ -74,6 +109,8 @@ public class Database {
         }
     }
 
+    // Updates the player count so that the bungee doesn't send
+    // too many and cause them to get kicked.
     public void updatePlayerCount() {
         redisConnection.sync().hset(
                 "server:" + serverConfig.getId(),
@@ -82,6 +119,7 @@ public class Database {
         );
     }
 
+    // Shutdown database stuff. Please call this fn or me and the databases will cry.
     public void close() {
         redisConnection.close();
         redisClient.shutdown();
--- src/main/java/network/reflected/rfnetapi/ServerConfig.java
+++ src/main/java/network/reflected/rfnetapi/ServerConfig.java
@@ -1,45 +1,99 @@
 package network.reflected.rfnetapi;
 
-import io.lettuce.core.RedisURI;
+import org.bukkit.configuration.file.YamlConfiguration;
 
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.List;
 
 public class ServerConfig {
     private String missingMsg = "";
-    private File bootConfig;
+    private boolean valid = true;
+    private boolean finishedLoad;
+    private File bootConfigFile;
+    private YamlConfiguration bootConfig;
 
     public ServerConfig() {
         // TODO: https://bukkit.org/threads/bukkits-yaml-configuration-tutorial.42770/
-        bootConfig = new File("./bootconfig.yml");
+        bootConfigFile = new File("./bootconfig.yml");
+
+        if(!bootConfigFile.exists()){
+            //noinspection ResultOfMethodCallIgnored
+            bootConfigFile.getParentFile().mkdirs();
+            copy(getClass().getClassLoader().getResourceAsStream("bootconfig.yml"), bootConfigFile);
+        }
+
+        try {
+            bootConfig.load(bootConfigFile);
+        } catch (Exception e) {
+            missingMsg = "Error loading config file.";
+            e.printStackTrace();
+        }
+
+        finishedLoad = true;
     }
 
     public boolean isValid() {
         // Check and make sure that the server ID field contains only contains ABCabc123
         // (other values may break some redis stuff)
-        missingMsg = "Config checker not implemented yet.";
-        return false;
+        return !(valid && finishedLoad);
     }
 
-    /**
-     * Returns a message about what properties are not in the config.
-     * @return The message
-     */
     public String whatsMissing() {
         return missingMsg;
     }
 
     public String getId() {
+        return bootConfig.getString("server-id");
     }
 
     public String getArchetype() {
+        return bootConfig.getString("archetype");
     }
 
-    public RedisURI getRedisURI() {
+    public String getRedisURI() {
+        return bootConfig.getString("redis-lettuce-uri");
     }
 
     public String getMongoURI() {
+        return bootConfig.getString("mongo-uri");
     }
 
-    public String getMaxPlayers() {
+    public String getConnectionStrategy() {
+        return bootConfig.getString("player-connection-strategy");
+    }
+
+    public boolean isGlobalChatEnabled() {
+        return bootConfig.getBoolean("global-chat");
+    }
+
+    public int getRedisPort() {
+        return bootConfig.getInt("redis-port");
+    }
+
+    public boolean isRandomizedMapsEnabled() {
+        return bootConfig.getBoolean("randomize-maps");
+    }
+
+    public List<String> getMaps() {
+        return bootConfig.getStringList("maps");
+    }
+
+    // Stolen from bukkit forums if it breaks then idk
+    private void copy(InputStream in, File file) {
+        try {
+            OutputStream out = new FileOutputStream(file);
+            byte[] buf = new byte[1024];
+            int len;
+            while((len=in.read(buf))>0){
+                out.write(buf,0,len);
+            }
+            out.close();
+            in.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
